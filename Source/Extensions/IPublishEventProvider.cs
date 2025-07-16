0dcdfc0dcec8396f72f8ec88e640e228d4a4cb6b
Jeremy El Bez <jelbez@genetec.com>
2025-07-16 14:47:39 -0400

    Allow override start point for MqttRouter

---
+++ Source/Extensions/IPublishEventProvider.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Atlas Lift Tech Inc. All rights reserved.
+
+using MQTTnet.Server;
+using System;
+using System.Threading.Tasks;
+
+namespace MQTTnet.AspNetCore.Routing;
+
+/// <summary>
+/// OnPublish provider allowing to trigger the Routing flow
+/// </summary>
+public interface IPublishEventProvider
+{
+    /// <summary>
+    /// OnPublishAsync will by the MqttRouter
+    /// </summary>
+    event Func<InterceptingPublishEventArgs, Task> OnPublishAsync;
+}
